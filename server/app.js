753bd387ab5c461c9bfb24c1d40a5b061652169f
verdeverzi <verdeverzi@yahoo.com>
2023-06-14 11:06:04 +0200

    changes

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:[ 'http://localhost:3000', 'https://petadoption-rescueme.onrender.com'],
+    origin:[ 'http://localhost:3000', 'https://vercel.com/verdeverzi/pet-adoption-web-app2/5ocvxp14Gfhfp4QCJWtnAERT5PxU'],
     credentials: true,
   })
 );
