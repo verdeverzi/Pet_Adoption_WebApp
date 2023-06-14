a3bf3837c5c2a00e1674f28341d50dc5d57dc6f2
verdeverzi <verdeverzi@yahoo.com>
2023-06-14 11:32:55 +0200

    changes

--- server/app.js
+++ server/app.js
@@ -17,9 +17,10 @@ const app = express();
 
 var corsOptions = {
   origin: ['http://localhost:3000', 'https://pet-adoption-web-app2-hvxpsfkr3-verdeverzi.vercel.app'], // add more origins if needed
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+  preflightContinue: true, // handles preflight requests
+  credentials: true // enabling cookies (since you're using `cookieParser`)
 };
-
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
